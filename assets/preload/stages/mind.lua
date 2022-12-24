4237d90240cc2ed158287e75923f32eef5104d23
Xkxksixickzociv <119426594+Xkxksixickzociv@users.noreply.github.com>
2022-12-25 00:34:05 +0200

    Add files via upload

---
+++ assets/preload/stages/mind.lua
@@ -0,0 +1,14 @@
+function onCreate()
+
+
+	makeLuaSprite('final', 'final-part', 0, 0)
+	addLuaSprite('final')
+
+	makeLuaSprite('fleetway', 'fleet', 0, 0)
+	addLuaSprite('fleetway')
+
+	makeLuaSprite('sonic', 'sonicbg', 0, 0)
+	addLuaSprite('sonic')
+
+
+end
